b62b371f92bdb251c0d18249dce60a8bd104e066
Adam Romano <adamromano@Adams-iMac.lan>
2021-12-26 22:29:41 -0600

    adding text to skill pages

---
+++ utils/about.js
@@ -0,0 +1,9 @@
+const About = {
+name: `Adam Romano`,
+intro: `I'm Adam Romano. I am a hardworking and ambitious individual with a great passion for software engineering.`,
+bio: `I am a hardworking and ambitious individual with a great passion for software development. I am currently studying Full-Stack Software engineering at UT, I am also perparing myself for my MS degree in Software Engineering. I love learning new skills and sharing my knowledge with others. I'm also working on expanding my knowedge to include several Back-End programming languages.
+
+. `,
+}
+
+export default About
