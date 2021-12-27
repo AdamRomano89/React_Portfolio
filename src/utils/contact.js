cac51e819b493633d92da6d39f692772c3cd0cca
Adam Romano <adamromano@Adams-iMac.lan>
2021-12-26 21:12:40 -0600

    initial setup

--- src/utils/contact.js
+++
@@ -1,9 +0,0 @@
-
-const Contact = {
-linkedin: `https://www.linkedin.com/in/adam-romano-9b7279186/`,
-mysite: `https://www.adamromano.net`,
-github: `https://github.com/adamromano89/`
-}
-
-
-export default Contact
--- src/utils/skillsBE.js
+++
@@ -1,7 +0,0 @@
-
-const SkillsBE = {
-
-    skills:[ `Node.js, JavaScript, Express.js, SQL, NoSQL, MERN`]
-    }
-
-export default SkillsBE
