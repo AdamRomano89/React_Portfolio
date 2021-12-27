3b22a8bf1973a949bb9a1d4a6b8b6a4db61c5d58
Adam Romano <adamromano@Adams-iMac.lan>
2021-12-26 21:10:34 -0600

    initial setup

---
+++ src/utils/contact.js
@@ -0,0 +1,9 @@
+
+const Contact = {
+linkedin: `https://www.linkedin.com/in/adam-romano-9b7279186/`,
+mysite: `https://www.adamromano.net`,
+github: `https://github.com/adamromano89/`
+}
+
+
+export default Contact
